73084b25fe32481e64925dc1fcecf32cedff4939
onedou <onedou@126.com>
2019-05-07 09:19:51 +0800

    update

--- Paracraft.app/Contents/Resources/assets/npl_packages/ParacraftBuildinMod/Mod/NplCadLibrary/test/test_2dboolean.lua
+++
@@ -1,5 +0,0 @@
-rotate(45);
-intersection();
-scale({2,2});
-square():translate(0.5,0.5);
-circle();
--- Paracraft.app/Contents/Resources/assets/npl_packages/ParacraftBuildinMod/Mod/ParaXExporter/package_init.lua
+++
@@ -1,5 +0,0 @@
-
-log(NPL.filename().." loaded\n")
--- NPL.load("(gl)script/apps/Aries/Creator/Game/Mod/ModManager.lua");
---local ModManager = commonlib.gettable("Mod.ModManager");
---ModManager:GetLoader():AddSystemModule("ParaXExporter", {displayName = "ParaX 3D模型导出"});
