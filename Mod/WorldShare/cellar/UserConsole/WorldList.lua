70d4b9159919d1749e9922976b5cbfb9d466de91
onedou <onedou@126.com>
2019-06-19 14:05:05 +0800

    remove project id when user destory project on keepwork

--- Mod/WorldShare/cellar/UserConsole/WorldList.lua
+++ Mod/WorldShare/cellar/UserConsole/WorldList.lua
@@ -330,10 +330,19 @@ function WorldList:SyncWorldsList(callback)
             end
 
             if (not isExist) then
-                currentWorld = LItem
+                local currentWorld = LItem
                 currentWorld.modifyTime = self:UnifiedTimestampFormat(currentWorld.writedate)
                 currentWorld.text = currentWorld.foldername
                 currentWorld.status = 1 --仅本地
+
+                if currentWorld.kpProjectId then
+                    currentWorld.kpProjectId = nil
+
+                    local tag = WorldCommon.LoadWorldTag(currentWorld["worldpath"])
+                    tag.kpProjectId = nil
+                    LocalService:SetTag(currentWorld["worldpath"], tag)
+                end
+
                 compareWorldList:push_back(currentWorld)
             end
         end
