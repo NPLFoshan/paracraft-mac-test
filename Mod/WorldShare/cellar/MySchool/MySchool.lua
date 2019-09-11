9974a7a7530c02b771f121bc396ab3d371ae6fbf
onedou <onedou@126.com>
2019-09-11 17:37:48 +0800

    add my school page

---
+++ Mod/WorldShare/cellar/MySchool/MySchool.lua
@@ -0,0 +1,16 @@
+--[[
+Title: my school page
+Author(s):  big
+Date: 2019.09.11
+Desc: 
+use the lib:
+------------------------------------------------------------
+local MySchool = NPL.load("(gl)Mod/WorldShare/cellar/MySchool/MySchool.lua")
+------------------------------------------------------------
+]]
+
+local MySchool = NPL.export()
+
+function MySchool:Show()
+    
+end
