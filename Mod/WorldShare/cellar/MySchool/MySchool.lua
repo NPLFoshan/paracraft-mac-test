694f8617046d36682cb05550c075239a4b578307
onedou <onedou@126.com>
2019-09-16 10:54:34 +0800

    update

--- Mod/WorldShare/cellar/MySchool/MySchool.lua
+++ Mod/WorldShare/cellar/MySchool/MySchool.lua
@@ -9,8 +9,38 @@ local MySchool = NPL.load("(gl)Mod/WorldShare/cellar/MySchool/MySchool.lua")
 ------------------------------------------------------------
 ]]
 
+local Utils = NPL.load("(gl)Mod/WorldShare/helper/Utils.lua")
+local Store = NPL.load("(gl)Mod/WorldShare/store/Store.lua")
+local KeepworkService = NPL.load("(gl)Mod/WorldShare/service/KeepworkService.lua")
+
 local MySchool = NPL.export()
 
 function MySchool:Show()
-    
+    local params = Utils:ShowWindow(870, 650, "Mod/WorldShare/cellar/MySchool/MySchool.html", "MySchool")
+
+    params._page:CallMethod("nplbrowser_instance", "SetVisible", true)
+
+    params._page.OnClose = function()
+        Store:Remove('page/MySchoolPage')
+        params._page:CallMethod("nplbrowser_instance", "SetVisible", false)
+    end
+end
+
+function MySchool:SetPage()
+    Store:Set('page/MySchoolPage', document:GetPageCtrl())
+end
+
+function MySchool:Close()
+    local MySchoolPage = Store:Get('page/MySchoolPage')
+
+    if MySchoolPage then
+        MySchoolPage:CloseWindow()
+    end
+end
+
+function MySchool.GetUrl()
+    local token = Store:Get("user/token") or ''
+    local url = KeepworkService:GetKeepworkUrl() .. '/p/org/home?port=8099&token=' .. token
+
+    return url
 end
