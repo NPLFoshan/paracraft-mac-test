4f06eab841e6fa4b14f6d48668b316be02ff820d
onedou <onedou@126.com>
2020-10-18 16:54:31 +0800

    update

--- Mod/WorldShare/cellar/Panorama/Panorama.lua
+++ Mod/WorldShare/cellar/Panorama/Panorama.lua
@@ -10,10 +10,28 @@ local Panorama = NPL.load("(gl)Mod/WorldShare/cellar/Panorama/Panorama.lua")
 ------------------------------------------------------------
 ]]
 
+-- lib
+local Screen = commonlib.gettable("System.Windows.Screen")
+
 local Panorama = NPL.export()
 
 function Panorama:ShowCreate()
-    local params = Mod.WorldShare.Utils.ShowWindow(600, 420, "(ws)Panorama/Create.html", "Mod.WorldShare.Panorama.Create")
+    local width = Screen:GetWidth()
+    local height = Screen:GetHeight()
+
+    local scaleWidth = width * 0.9
+    local scaleHeight = height * 0.9
+
+    local params = Mod.WorldShare.Utils.ShowWindow(
+        scaleWidth,
+        scaleHeight,
+        format("Mod/WorldShare/cellar/Panorama/Create.html?width=%d&height=%d", scaleWidth, scaleHeight),
+        "Mod.WorldShare.Panorama.Create",
+        nil,
+        nil,
+        nil,
+        false
+    )
 end
 
 function Panorama:ShowPreview()
@@ -22,4 +40,8 @@ end
 
 function Panorama:ShowShare()
     local params = Mod.WorldShare.Utils.ShowWindow(600, 420, "(ws)Panorama/Share.html", "Mod.WorldShare.Panorama.Share")
+end
+
+function Panorama:StartShooting()
+    self:ShowPreview()
 end
